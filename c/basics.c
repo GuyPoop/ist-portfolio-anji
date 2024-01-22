ffa91095f1da0640d643586c43576b7863d6c3eb
GuyPoop <2027relova.anji@fcboe.org>
2024-01-22 14:47:57 -0500

    Created basics

---
+++ c/basics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 	
+{
+
+printf("This is my c basics file\n");
+int num;
+printf("What is your number? ");
+scanf("%i", &num);
+printf("Your number was %i\n", num);
+
+return 0;
+}
