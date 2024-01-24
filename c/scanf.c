c9b522ab2cb0e25980a132b981b1be1bba713e2f
GuyPoop <2027relova.anji@fcboe.org>
2024-01-24 14:18:12 -0500

    made scanf

---
+++ c/scanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int age;
+	float gpa;
+	char grade;
+	printf("What is your age: ");
+	scanf("%i", &age);
+	printf("What is your gpa: ");
+	scanf("%f", &gpa);
+	printf("What is your grade: ");
+	scanf(" %c", &grade);
+	printf("Your age is %i\n", age);
+	printf("Your gpa is %f\n", gpa);
+	printf("Your grade is %c\n", grade);
+return 0;
+}
+
